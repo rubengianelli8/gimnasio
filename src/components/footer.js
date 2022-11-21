832ac5b8f03a72f982a1adca2056898f75626285
Ruben Gianelli <ruben.gianelli@tiarg.com.ar>
2022-11-21 18:37:31 -0300

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Logo from "@/assets/images/logo-gym.png";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import { BsFillHouseDoorFill, BsFillTelephoneFill } from "react-icons/bs";
+
+const Footer = () => {
+  return (
+    <footer className="grid gap-y-[20px] md:gap-y-0 grid-cols-1 md:grid-cols-3 w-screen bg-black text-white px-[30px] py-[30px] place-content-center text-[14px]">
+      <div className="flex flex-col justify-center">
+        <p className="mx-auto">Copyright &copy; 2022 por Rubén Gianelli</p>
+        <div className="mt-3 flex justify-center gap-x-4">
+          <span className="text-[20px] p-[10px] rounded-full bg-gray-800 border hover:text-primary hover:border-primary cursor-pointer">
+            <FaFacebookF />
+          </span>
+          <span className="text-[20px] p-[10px] rounded-full bg-gray-800 border hover:text-primary hover:border-primary cursor-pointer">
+            <FaInstagram />
+          </span>
+          <span className="text-[20px] p-[10px] rounded-full bg-gray-800 border hover:text-primary hover:border-primary cursor-pointer">
+            <FaTwitter />
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-2 justify-center items-center">
+        <div className="flex items-center gap-x-2 ">
+          <span className="text-[20px] p-[10px] text-white bg-primary rounded-full">
+            <BsFillHouseDoorFill />
+          </span>
+          <p>La valle 779, San Cristobal, Santa Fe</p>
+        </div>
+        <div className="flex items-center gap-x-2 ">
+          <span className="text-[20px] p-[10px] text-white bg-primary rounded-full">
+            <BsFillTelephoneFill />
+          </span>
+          <p>3408 67-0960</p>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Image src={Logo} width={250} height={180} alt="Gimnasio en Santa Fe" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import HomeContainer from "@/containers/index";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomeContainer />
+      <div className="h-[200px]"></div>
+      <Footer />
     </>
   );
 }
