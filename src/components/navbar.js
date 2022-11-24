b0fef8171b13649da0eae41da1ba285791366eba
Ruben Gianelli <ruben.gianelli@tiarg.com.ar>
2022-11-24 18:03:26 -0300

    animacion de navbar mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ import { IoIosCloseCircle } from "react-icons/io";
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(true);
   return (
-    <nav className="h-[80px] max-w-screen px-[20px] bg-black text-white flex items-center">
+    <nav className="h-[80px] max-w-screen px-[20px] bg-black text-white flex items-center ">
       <ul className="hidden md:flex h-full gap-x-4 items-center w-full justify-center text-[20px] font-medium ">
         <li className="hover:text-primary hover:underline cursor-pointer">
           Inicio
@@ -19,31 +19,35 @@ const Navbar = () => {
           Contacto
         </li>
       </ul>
-      {showNavbar && (
-        <ul className="bg-primary absolute top-0 right-0 flex flex-col md:hidden h-screen gap-y-4  w-[200px] justify-start text-[17px] font-medium pl-[10px] animation-mia">
-          <div className="flex w-full border-b-2 items-center  mt-[10px]">
-            <h3 className="text-[20px] uppercase font-semibold ">Menu</h3>
-            <span
-              className="ml-auto text-[25px] mr-2 cursor-pointer"
-              onClick={() => setShowNavbar(false)}
-            >
-              <IoIosCloseCircle />
-            </span>
-          </div>
-          <li className="hover:text-primary hover:underline cursor-pointer">
-            Inicio
-          </li>
-          <li className="hover:text-primary hover:underline cursor-pointer">
-            Instalaciones
-          </li>
-          <li className="hover:text-primary hover:underline cursor-pointer">
-            Actividades
-          </li>
-          <li className="hover:text-primary hover:underline cursor-pointer">
-            Contacto
-          </li>
-        </ul>
-      )}
+      <ul
+        className={`${
+          showNavbar ? "translate-x-0" : "translate-x-[200px] opacity-0"
+        }
+             transtion-all ease-in-out duration-500 bg-primary fixed top-0 right-0 flex flex-col md:hidden h-screen gap-y-4  w-[200px] justify-start text-[17px] font-medium pl-[10px] z-50`}
+      >
+        <div className="flex w-full border-b-2 items-center  mt-[10px]">
+          <h3 className="text-[20px] uppercase font-semibold ">Menu</h3>
+          <span
+            className="ml-auto text-[25px] mr-2 cursor-pointer"
+            onClick={() => setShowNavbar(false)}
+          >
+            <IoIosCloseCircle />
+          </span>
+        </div>
+        <li className="hover:text-primary hover:underline cursor-pointer">
+          Inicio
+        </li>
+        <li className="hover:text-primary hover:underline cursor-pointer">
+          Instalaciones
+        </li>
+        <li className="hover:text-primary hover:underline cursor-pointer">
+          Actividades
+        </li>
+        <li className="hover:text-primary hover:underline cursor-pointer">
+          Contacto
+        </li>
+      </ul>
+
       <span
         className="ml-auto text-[30px] hover:text-primary hover:underline cursor-pointer hover:text-[34px] md:hidden"
         onClick={() => setShowNavbar(true)}
